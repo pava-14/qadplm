ca94d6f8c902bab7081dd43c7c2a27bd1c3d26ab
Alexander Pavlenko <pava@bk.ru>
2020-08-26 16:53:33 +0700

    OrderPage: Fields (Error) Added

--- src/test/java/ru/netology/pages/OrderPage.java
+++ src/test/java/ru/netology/pages/OrderPage.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.SelenideElement;
 import ru.netology.data.Card;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -13,13 +12,24 @@ public class OrderPage {
     private final String buttonContinueCaption = "Продолжить";
     private final String approvedMessage = "Операция одобрена Банком.";
     private final String declinedMessage = "Ошибка! Банк отказал в проведении операции.";
-    private final String fieldSelector = ".input__control";
+    private final String fieldErrorSelector = ".input__sub";
+    private final String fieldInputSelector = ".input__control";
+    private final String fieldCardNumberErrorText = "";
+    private final String fieldMonthErrorText = "";
+    private final String fieldYearErrorText = "";
+    private final String fieldOwnerErrorText = "";
+    private final String fieldCvcCvvErrorText = "";
 
-    private final SelenideElement fieldCardNumber = $$(fieldSelector).get(0);
-    private final SelenideElement fieldMonth = $$(fieldSelector).get(1);
-    private final SelenideElement fieldYear = $$(fieldSelector).get(2);
-    private final SelenideElement fieldOwner = $$(fieldSelector).get(3);
-    private final SelenideElement fieldCvcCvv = $$(fieldSelector).get(4);
+    private final SelenideElement fieldCardNumber = $$(fieldInputSelector).get(0);
+    private final SelenideElement fieldCardNumberError = $$(fieldErrorSelector).get(0);
+    private final SelenideElement fieldMonth = $$(fieldInputSelector).get(1);
+    private final SelenideElement fieldMonthError = $$(fieldErrorSelector).get(1);
+    private final SelenideElement fieldYear = $$(fieldInputSelector).get(2);
+    private final SelenideElement fieldYearError = $$(fieldErrorSelector).get(2);
+    private final SelenideElement fieldOwner = $$(fieldInputSelector).get(3);
+    private final SelenideElement fieldOwnerError = $$(fieldErrorSelector).get(3);
+    private final SelenideElement fieldCvcCvv = $$(fieldInputSelector).get(4);
+    private final SelenideElement fieldCvcCvvError = $$(fieldErrorSelector).get(4);
 
     public void setCardFields(Card card) {
         fieldCardNumber.setValue(card.getCardNumber());
