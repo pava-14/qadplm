a434be16f3e1092520e3fb361b70a5dbb3ad8728
Alexander Pavlenko <pava@bk.ru>
2020-08-31 16:48:13 +0700

    DbHelper Class Added

--- src/test/java/ru/netology/data/DbHelper.java
+++ src/test/java/ru/netology/data/DbHelper.java
@@ -1,4 +1,8 @@
 package ru.netology.data;
 
 public class DbHelper {
+    private final static String url = "jdbc:mysql://localhost:3306/app";
+    private final static String user = "app";
+    private final static String password = "pass";
+
 }
