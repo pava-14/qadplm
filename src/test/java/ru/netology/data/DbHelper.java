82c55a087c82bbc06a64bfd2d618ca94807ca3d7
Alexander Pavlenko <pava@bk.ru>
2020-08-31 16:55:39 +0700

    DbHelper Class Fields Added

--- src/test/java/ru/netology/data/DbHelper.java
+++ src/test/java/ru/netology/data/DbHelper.java
@@ -1,8 +1,42 @@
 package ru.netology.data;
 
+/*
+credit_request_entity;
++---------+--------------+------+-----+---------+-------+
+| Field   | Type         | Null | Key | Default | Extra |
++---------+--------------+------+-----+---------+-------+
+| id      | varchar(255) | NO   | PRI | NULL    |       |
+| bank_id | varchar(255) | YES  | UNI | NULL    |       |
+| created | datetime(6)  | YES  |     | NULL    |       |
+| status  | varchar(255) | YES  |     | NULL    |       |
++---------+--------------+------+-----+---------+-------+
+
+payment_entity;
++----------------+--------------+------+-----+---------+-------+
+| Field          | Type         | Null | Key | Default | Extra |
++----------------+--------------+------+-----+---------+-------+
+| id             | varchar(255) | NO   | PRI | NULL    |       |
+| amount         | int(11)      | NO   |     | NULL    |       |
+| created        | datetime(6)  | YES  |     | NULL    |       |
+| status         | varchar(255) | YES  |     | NULL    |       |
+| transaction_id | varchar(255) | YES  | UNI | NULL    |       |
++----------------+--------------+------+-----+---------+-------+
+
+order_entity;
++------------+--------------+------+-----+---------+-------+
+| Field      | Type         | Null | Key | Default | Extra |
++------------+--------------+------+-----+---------+-------+
+| id         | varchar(255) | NO   | PRI | NULL    |       |
+| created    | datetime(6)  | YES  |     | NULL    |       |
+| credit_id  | varchar(255) | YES  |     | NULL    |       |
+| payment_id | varchar(255) | YES  |     | NULL    |       |
++------------+--------------+------+-----+---------+-------+
+ */
 public class DbHelper {
     private final static String url = "jdbc:mysql://localhost:3306/app";
     private final static String user = "app";
     private final static String password = "pass";
+
+
 
 }
