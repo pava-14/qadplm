22ef429a328bc96144ea580c4bfc68e83b2fa53b
Alexander Pavlenko <pava@bk.ru>
2020-08-31 17:13:21 +0700

    DbHelper: clearTable method Added

--- src/test/java/ru/netology/data/DbHelper.java
+++ src/test/java/ru/netology/data/DbHelper.java
@@ -1,5 +1,12 @@
 package ru.netology.data;
 
+import lombok.val;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /*
 credit_request_entity;
 +---------+--------------+------+-----+---------+-------+
@@ -37,6 +44,21 @@ public class DbHelper {
     private final static String user = "app";
     private final static String password = "pass";
 
-
+    /*
+    "TRUNCATE TABLE credit_request_entity;
+    TRUNCATE TABLE payment_entity;
+    TRUNCATE TABLE order_entity;"
+     */
+    public static void clearTable() {
+        val deleteAuthCodeSQL = " DELETE FROM auth_codes;";
+        val runner = new QueryRunner();
+        try {
+            try (val conn = DriverManager.getConnection(url, user, password)) {
+                runner.execute(conn, deleteAuthCodeSQL, new ScalarHandler<>());
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 
 }
