60bec6bd0904ab1ccd9be7c3502fbd7baff1362b
Alexander Pavlenko <pava@bk.ru>
2020-10-13 13:08:38 +0700

    Code Review: Teacher's Comment #7

--- src/test/java/ru/netology/data/SutData.java
+++ src/test/java/ru/netology/data/SutData.java
@@ -1,8 +1,11 @@
 package ru.netology.data;
 
-public class SutData {
+public final class SutData {
     public static final int amount = 4500000;
     public static final String approvedStatus = "APPROVED";
     public static final String declinedStatus = "DECLINED";
     public static final String startPageUrl = "http://localhost:8080";
+
+    private SutData() {
+    }
 }
